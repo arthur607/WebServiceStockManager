c303a5f8032c21861e4fc15a486fa93b38b5b03c
Arthur Garcia <arthurrodriguesg1@gmail.com>
2021-08-17 12:00:15 -0300

    start project

---
+++ src/main/java/com/project/salsaModas/SalsaModas/SalsaModasApplication.java
@@ -0,0 +1,13 @@
+package com.project.salsaModas.SalsaModas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsaModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsaModasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/salsaModas/SalsaModas/SalsaModasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.salsaModas.SalsaModas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsaModasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
